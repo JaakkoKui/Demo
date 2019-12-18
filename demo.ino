9e56425f2a071357113fefeb5dde654e48743032
Jaakko Kuivasniemi <jaakko.kuivasniemi@ictpro.labra>
2019-12-18 12:56:29 +0200

    Siivous

--- demo.ino
+++ demo.ino
@@ -1,11 +1,12 @@
 #define LED 6
 #define PAINIKE 5
-#define TASO1 180
-#define TASO2 100
-#define LEDON HIGH
-#define LEDOFF LOW
+#define TASO1 100
+#define TASO2 40
+#define LEDON 255
+#define LEDOFF 0
 #define FILTER 20
 int oldState; // Painikkeen tilojen seurantaa varten
+int valaistusTaso = LEDOFF;
 void setup () {
    pinMode (LED, OUTPUT);
    pinMode (PAINIKE, INPUT_PULLUP);
@@ -23,9 +24,18 @@ bool buttonPressed () {
   delay (FILTER);
   return retVal;
 }
-
+void changeState(){
+       Serial.println ("Khyy");
+      switch (valaistusTaso){
+        case LEDOFF:valaistusTaso=LEDON;break;
+        case LEDON:valaistusTaso=TASO1;break;
+        case TASO1:valaistusTaso=TASO2;break;
+        case TASO2:valaistusTaso=LEDOFF;break;
+      }
+      analogWrite (LED, valaistusTaso);
+}
 void loop() {
   if (buttonPressed() ){
-      Serial.println ("Khyy");
+     changeState();
   }
 }
